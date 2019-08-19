9b6b5f19fa9cabf60f2be53fa579474845d2accb
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-19 02:01:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,12 @@ class Application
     if req.path=="/items"
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+         resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+      
     else
       resp.write "Route not found"
       resp.status = 404
