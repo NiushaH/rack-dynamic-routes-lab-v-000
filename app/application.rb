95b9580b5f52af8bc1dd6db6d2e50a9d67cc816c
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-19 02:01:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
 
-  @@items = []
+  @@item = []
 
   def call(env)
     resp = Rack::Response.new
@@ -8,7 +8,7 @@ class Application
  
     if req.path=="/items"
       item_name = req.path.split("/items/").last
-      if item = @@items.find{|i| i.name == item_name}
+      if item = @@item.find{|i| i.name == item_name}
          resp.write item.price
       else
         resp.write "Item not found"
